bca60c12c71032658e63935fcde2eb0044620316
JPedrys <61942688+JPedrys@users.noreply.github.com>
2020-03-15 13:40:08 -0400

    First Commit - WIP
    This project is functional, but not all the features are completely added.

---
+++ app.js
@@ -0,0 +1,27 @@
+const geoCode = require('./utils/geocode');
+const forecast = require('./utils/forecast');
+
+const location = process.argv[2];
+
+if(location){
+    geoCode(location, (error, data) =>
+    {
+        if(error)
+        {
+            return console.log(error);
+        }
+        forecast(data.latitude, data.longitude, (error, forecastData) => 
+        {
+            if(error)
+            {
+                return console.log(error);
+            }
+
+            console.log(data.location);
+            console.log(forecastData);
+        })
+    })
+}else
+{
+    console.log('A location was not provided.');
+}
